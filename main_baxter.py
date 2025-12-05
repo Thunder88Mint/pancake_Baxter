ca232db25fc5db3145d8e780b475a1dd0dde42af
Cody G <grecody@byu.edu>
2025-12-05 11:13:02 -0700

    Copied code from main_sim to main_baxter

--- main_baxter.py
+++ main_baxter.py
@@ -3,6 +3,8 @@ from setup_baxter import setup_baxter
 from flip_pancake_sequence import flip_pancake_sequence
 from travel2pancake import travel2pancake
 from move_baxter import move_baxter
+from interpolate_between_positions_in_joint_space import interpolate_between_positions_in_joint_space
+from move_baxter_in_visual import move_baxter_in_visual
 
 import kinematics_key_hw07 as kin
 import transforms_key_hw04 as tr
@@ -16,11 +18,12 @@ import rospy
 if __name__ == "__main__":
 
     # Inputs
-    pancakePosition_in_0 = np.array([1,0,0])
-    q = [0,0,0,0,0,0,0]
+    pancakePosition_in_0 = np.array([1,-0.5,0])
+    platePosition_in_0 = np.array([1,-1,0])
+    q_initial = [np.pi/4,0,0,0,0,0,0]
 
     # Test IK_full_pose
-    K = np.eye(6) * 0.5
+    K = np.eye(6) * 0.25
     debug = False
 
     arm = setup_baxter()
@@ -30,24 +33,26 @@ if __name__ == "__main__":
     baxterFeedRateHz = 500
     
     # Step 0: initial position
-    T0 = arm.fk(q)
+    T0 = arm.fk(q_initial,base=True)
     p0 = T0[:3,3]
     input('press Enter to see next iteration')
 
     # Step 1: move to pancake
-    travel2pancake(arm)
-    T_byPancake = tr.se3(p=[0.8,0,0])
-    q1, e, count, successful, msg = arm.ik_full_pose(T_byPancake, q, K=K, max_iter=10000, method='pinv', debug=debug, debug_step=debug)
-    input('press Enter to see next iteration')
+    T_byPancake = tr.se3(p=[0.8,-0.5,0])
+    q1, e, count, successful, msg = arm.ik_full_pose(T_byPancake, q_initial, K=K, max_iter=20000, method='pinv', debug=debug, debug_step=debug)
+    qs = interpolate_between_positions_in_joint_space(q_initial, q1, 100)
+    q_current = qs[-1]
+
     move_baxter(limb, q1, baxterFeedRateHz)
+    input('press Enter to see next iteration')
 
     # Step 2: Flip Pancake
     Ts = flip_pancake_sequence(arm, T_byPancake, pancakeLocation=pancakePosition_in_0)
     for i in range(len(Ts)):
-        q1, e, count, successful, msg = arm.ik_full_pose(Ts[i], q1, K=K, max_iter=10000, method='pinv', debug=debug, debug_step=debug)
-        # viz.add_frame(Ts[i])
-        input('press Enter to see next iteration')
-        move_baxter(limb, q1, baxterFeedRateHz)
+        q_goal, e, count, successful, msg = arm.ik_full_pose(Ts[i], q_current, K=K, max_iter=10000, method='pinv', debug=debug, debug_step=debug)
+        qs = interpolate_between_positions_in_joint_space(q_current, q_goal, steps=100)
+        q_current = qs[-1]
 
-    
+        input('press Enter to see next iteration')
+        move_baxter(limb, q1, baxterFeedRateHz)    
 
